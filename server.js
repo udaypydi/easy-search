21c3991f0c9c6ff57d0ed4f593d41f4c017dc3dd
uday bhaskhar <udaybhaskhar@udays-Mac-mini.local>
2021-05-17 17:21:26 +0530

    feat(apis): add apis

--- server.js
+++ server.js
@@ -1,112 +1,143 @@
-const elasticsearch = require('elasticsearch');
-const express = require('express');
-const fetch = require('node-fetch');
-const { nanoid } = require('nanoid');
 
-const app = express();
+const express = require("express")
+const bodyParser = require("body-parser")
+const elasticsearch = require("elasticsearch");
+const fetch = require('node-fetch');
+const app = express()
+app.use(bodyParser.json())
+
+
 
 const esClient = new elasticsearch.Client({
     host: '127.0.0.1:9200',
     log: 'error'
 });
 
-
-const bulkIndex = function bulkIndex(index, type, data, cb) {
+function bulkIndex(index, type, data, cb) {
     let bulkBody = [];
     
-    esClient.indices.create({
-            index,
-        }, function(error, response, status) {
-            if (error) {
-                cb();
-            } else {
-                console.log(response);
-
-                data.forEach(item => {
-                    bulkBody.push({
-                      index: {
-                        _index: index,
-                        _type: type,
-                        _id: item.pid
-                      }
-                    });
-                
-                    bulkBody.push(item);
-                  });
-                
-                  esClient.bulk({body: bulkBody})
-                  .then(response => {
-                    console.log('here');
-                    let errorCount = 0;
-                    
-                    response.items.forEach(item => {
-                      if (item.index && item.index.error) {
-                        console.log(++errorCount, item.index.error);
-                      }
-                    });
-              
-                    cb(response);
-              
-                    console.log(
-                      `Successfully indexed ${data.length - errorCount}
-                       out of ${data.length} items`
-                    );
-                  })
-                  .catch(console.err);
-            }
+    data.forEach(item => {
+      bulkBody.push({
+        index: {
+          _index: index,
+          _type: type,
+          _id: item.pid
+        }
+      });
+  
+      bulkBody.push(item);
     });
-  };
   
-  const search = function search(index, body) {
-    return esClient.search({index: index, body: body});
+    esClient.bulk({body: bulkBody})
+    .then(response => {
+      console.log('here');
+      let errorCount = 0;
+      
+      response.items.forEach(item => {
+        if (item.index && item.index.error) {
+          console.log(++errorCount, item.index.error);
+        }
+      });
+
+      cb(response)
+
+      console.log(
+        `Successfully indexed ${data.length - errorCount}
+         out of ${data.length} items`
+      );
+    })
+    .catch(console.err);
   };
 
-  
-  const test = function test() {
-    fetch('https://raw.githubusercontent.com/appbaseio/cdn/dev/appbase/ecommerce_data.json')
+app.post('/create-mapping', (req,res) => {
+  const { index, mapping } = req.body;
+
+  esClient.indices.create({
+    index,
+    }, function(error, response, status) {
+
+        if (error) {
+          res.json(error);
+        }
+        else {
+          res.json(response);
+        }
+
+      // esClient.indices.putMapping({  
+      //   index,
+      //   type: 'products',
+      //   include_type_name: true,
+      //   body: {
+      //     properties: mapping,
+      //   }
+      // },function(err,resp,status){
+      //     if (err) {
+      //       res.json(err);
+      //     }
+      //     else {
+      //       res.json(resp);
+      //     }
+      // })
+
+    }
+  );
+});
+
+app.post('/bulk-sync', (req, res) => {
+    const { dataSource } = req.body;
+    
+    esClient.deleteByQuery({
+      index: 'catalog-data',
+      body: {
+        query: {
+          match_all: {},
+        }
+      }
+    }, (err, resp) => {
+      console.log('err', err);
+      if (!err) {
+        fetch(dataSource)
         .then(res => res.json())
         .then(json => {
-            bulkIndex('product_catalog_1', 'products', json, () => {
-                testSearch();
+            bulkIndex('catalog-data', 'products', json, (data) => {
+              res.json(data);
             });
-            
+        }).catch(err => {
+          res.json(err);
         })
-    
-  };
+      }
+    })
+});
 
 
-  const testSearch = () => {
+function search(index, body) {
+  return esClient.search({index: index, body: body});
+}
+
+app.get('/products', (req, res) => {
+  const { searchText } = req.query;
     let body = {
        "query": {
            "multi_match": {
-               query: "Interface",
+               query: searchText,
                fuzziness: 2,
                fields: ['product_name', 'description', 'categories']
            }
        }
     };
 
-    search('product_catalog_1', body)
+    search('catalog-data', body)
     .then(results => {
-      console.log(`found ${results.hits.total} items in ${results.took}ms`);
-      console.log(`returned article titles:`);
-      results.hits.hits.forEach(
-        (hit, index) => console.log(hit._source.pid)
-      )
+      console.log(results.hits.total.value);
+      const data = results.hits.hits.map(
+        (hit, index) => hit._source
+      );
+      res.json(data);
     })
     .catch(console.error);
-  };
-
-
-
-
-//   test();
-
-
-
-
-
-
-app.listen(3000, () => {
-    console.log('server started on PORT:', 3000);
 });
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("connected")
+});
+
